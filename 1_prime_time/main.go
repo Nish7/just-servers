432186980d7c00138c439d755fb0e35d7215a5ed
Nishil <nishilkapadia17@gmail.com>
2024-10-14 21:46:34 -0400

    chore: wroking impl

--- 1_prime_time/main.go
+++ 1_prime_time/main.go
@@ -5,40 +5,82 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math"
 	"net"
 )
 
-func isPrime(n int) bool {
-	return true
-}
-
-type RequestResponse struct {
+type Request struct {
 	Method string `json:"method"`
 	Number int    `json:"number"`
 }
 
+type Response struct {
+	Method string `json:"method"`
+	Prime  bool   `json:"prime"`
+}
+
 func handleRequest(c net.Conn) {
-	defer c.Close()
 	// handle decoding
+	defer c.Close()
 	reader := bufio.NewReader(c)
-	message, err := reader.ReadString('\n')
 
-	if err != nil {
-		log.Fatal(err)
+	for {
+		message, err := reader.ReadString('\n')
+
+		if err != nil {
+			c.Write([]byte(err.Error()))
+			break
+		}
+
+		fmt.Printf("Recieved: %v", message)
+
+		var request Request
+		err = json.Unmarshal([]byte(message), &request)
+
+		// handle validation
+		if err != nil {
+			c.Write([]byte(err.Error()))
+			break
+		}
+
+		if request.Method != "isPrime" {
+			c.Write([]byte("Request Method is not isPrime"))
+			break
+		}
+
+		// handle response
+		res := Response{Method: "isPrime", Prime: isPrime(request.Number)}
+		resJson, err := json.Marshal(res)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.Write([]byte(string(resJson) + "\n"))
+	}
+}
+
+func isPrime(n int) bool {
+	if n == 1 {
+		return false
 	}
 
-	log.Println(message)
-
-	var request RequestResponse
-	err = json.Unmarshal([]byte(message), &request)
-
-	if err != nil {
-		log.Fatal(err)
+	if n == 2 {
+		return true
 	}
 
-	fmt.Printf("Recieved: %v\n", request)
+	if n%2 == 0 {
+		return false
+	}
 
-	// handle malformed validaton
+	sqrtN := int(math.Sqrt(float64(n)))
+	for i := 3; i <= sqrtN; i += 2 {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
 }
 
 func main() {
@@ -54,14 +96,12 @@ func main() {
 	fmt.Println("Server is listening on port 8081...")
 
 	for {
-		// accept a connection
 		conn, err := l.Accept()
 
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		// use a go routine to handle the response
 		go handleRequest(conn)
 	}
 
